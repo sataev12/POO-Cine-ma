87f40628ddde6cd68dc03e8421a62a488c36f566
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-07 16:58:37 +0100

    Creation des classe et les lieson entre les differants classes

--- Index.php
+++ Index.php
@@ -1,3 +1,22 @@
 <?php
 
-spl_autoload_register();
+spl_autoload_register(function($class_name) {
+    require 'classes/'.$class_name . '.php';
+});
+
+
+
+
+
+
+$roleFilm = new Role("Han Solo");
+
+$genreFilm = new Genre("Science-fiction");
+$realisation = new Realisateur("George", "Lucas", "Masculin", "14-05-1944");
+
+$acteurFilm = new Acteur("Harrison", "Ford", "Masculin", "13-07-1942");
+$starWars = new Film("Star Wars Episode 4", "1977", "121", $realisation, $genreFilm, "synopsis");
+
+echo $starWars->getTitre();
+
+
